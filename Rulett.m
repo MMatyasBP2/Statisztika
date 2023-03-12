5fdb167d8243c0f7ac4093832c3dc71f78318013
Martinák Mátyás <99259193+MMatyasBP2@users.noreply.github.com>
2023-03-12 15:01:46 +0100

    Update Rulett.m

--- Rulett.m
+++ Rulett.m
@@ -1,35 +1,37 @@
-% Rulett szimuláció
-
-% Előkészítés
 clear;
 clc;
 
-% Paraméterek
-money = 1; % Pénzösszeg
-p_red = 18/38; % A piros színre való fogadás valószínűsége
-num_spins = 1000; % Pörgetések száma
+money = 1;
+p_red = 18/38;
+num_spins = 1000;
 
-% Rulett szimuláció
 results = randsample({'red','black'}, num_spins, true, [p_red, 1-p_red]);
 
-% Pénzösszeg változása az idő függvényében
 money_vec = cumsum(2*money*(strcmp(results,'red')-0.5));
 
-% Normális eloszlás illesztése
 mu = mean(money_vec);
 sigma = std(money_vec);
+
 x = -100:0.1:100;
 y = normpdf(x,mu,sigma);
 
-% Ábra
 figure;
 histogram(money_vec,50,'Normalization','pdf');
 hold on;
 plot(x,y,'r','LineWidth',2);
 plot([mu mu], [0 max(y)], 'g--','LineWidth',2);
-xlabel('Pénzösszeg');
-ylabel('Relatív gyakoriság');
-title('Rulett szimuláció');
+xlabel('Amount');
+ylabel('Relative frequency');
+title('Roulette simulation');
 
-% Ábrázolás
 hold off;
+
+median_money = median(money_vec);
+mad_money = mad(money_vec, 1);
+variance_money = var(money_vec);
+mean_money = mean(money_vec);
+
+disp(['Median: ' num2str(median_money)]);
+disp(['Median absolute deviation: ' num2str(mad_money)]);
+disp(['Variance: ' num2str(variance_money)]);
+disp(['Mean: ' num2str(mean_money)]);
